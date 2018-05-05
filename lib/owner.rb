632ca4cef5eaddc84fab47671db8d3cae9db87c5
Karl Amoguis <ktamoguis@gmail.com>
2018-05-05 06:00:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,12 @@
 class Owner
   # code goes here
   attr_reader :species
+  attr_accessor :name
 
   @@all = []
   @@count = 0
 
-  def initialize(species="human")
+  def initialize(species="human",pets = {:fishes => [], :dogs => [], :cats => []})
     @species = species
   end
 
@@ -20,6 +21,9 @@ class Owner
   def say_species
     "I am a human"
   end
+
+  
+    
 
 
 end
