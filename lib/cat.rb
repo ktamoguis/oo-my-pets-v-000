7c82ce7736fe105e46a5dea0f90a3760030b7343
Karl Amoguis <ktamoguis@gmail.com>
2018-05-05 05:50:11 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   attr_reader :name
   attr_accessor :mood
   # code goes here
-  def initialize(name, mood="nervous")
+  def initialize(name="crookshanks", mood="nervous")
     @name = name
     @mood = mood
   end
